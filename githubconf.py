8d06c5ae33fafa358d2203c99da3c8bdb6792a68
Peter Molfese <pmolfese@gmail.com>
2026-07-30 15:57:33 -0400

    working with gh pages

---
+++ githubconf.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+sys.path.append(os.curdir)
+from pelicanconf import *
+
+SITEURL = "https://nimh-cmn.github.io/cmn-website2026"
+RELATIVE_URLS = False
+DELETE_OUTPUT_DIRECTORY = True
