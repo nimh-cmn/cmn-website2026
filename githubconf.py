751c67895838eba83fa9fec337ea20bab1a594ec
Peter Molfese <pmolfese@gmail.com>
2026-07-31 12:10:36 -0400

    github pages

--- githubconf.py
+++ githubconf.py
@@ -4,6 +4,6 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = "https://nimh-cmn.github.io/cmn-website2026"
+SITEURL = "https://nimh-cmn.github.io"
 RELATIVE_URLS = False
 DELETE_OUTPUT_DIRECTORY = True
